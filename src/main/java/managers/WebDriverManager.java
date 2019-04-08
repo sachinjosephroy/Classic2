0ef242f171685cc4c7075a2041072e18ba6571bd
Sachin Roy <sachin.jr@gmail.com>
2019-04-07 22:28:47 -0400

    Initial commit

--- src/main/java/managers/WebDriverManager.java
+++ src/main/java/managers/WebDriverManager.java
@@ -12,8 +12,8 @@ public class WebDriverManager {
 	String browserversion;
 	
 	public WebDriverManager() {
-		String osName = System.getProperty("os.name");
-		if (osName.equalsIgnoreCase("windows 7")) {
+		String osName = System.getProperty("user.name");
+		if (osName.equalsIgnoreCase("sachinroy")) {
 			System.setProperty("webdriver.chrome.driver", "C:\\Selenium Grid\\chromedriver.exe");
 			driver = new ChromeDriver();
 			Capabilities cap = ((ChromeDriver) driver).getCapabilities();
@@ -21,9 +21,11 @@ public class WebDriverManager {
 			browserversion = cap.getVersion();
 			test.setDescription("Operating System: " + osName.toUpperCase() + ", " + "<br />" + "Browser: " + browsername + " " + browserversion);*/
 		}
-		else if (osName.equalsIgnoreCase("windows 10")) {
+		else if (osName.equalsIgnoreCase("simir")) {
 			System.setProperty("webdriver.gecko.driver", "C:\\Selenium Grid\\geckodriver.exe");
 			driver = new FirefoxDriver();
+			browsername = System.getProperty("user.name");
+			System.out.println("OS User name is: " + browsername);
 			Capabilities cap = ((FirefoxDriver) driver).getCapabilities();
 			//browsername = cap.getBrowserName().toUpperCase();
 			
@@ -34,7 +36,7 @@ public class WebDriverManager {
 			
 			//test.setDescription("Operating System: " + osName.toUpperCase() + ", " + "<br />" + "Browser: " + browsername);
 		}
-		this.driver = driver;	
+		this.driver = driver;
 	}
 	
 	public WebDriver getDriver() {
